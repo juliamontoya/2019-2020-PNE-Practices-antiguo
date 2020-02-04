f63043967f12fe1900b55db24d60f7aead3e3999
jmontoya <juliamontoyajimenez@gmail.com>
2020-02-04 16:20:41 +0100

    just another test...

--- hello /hello.py
+++ hello /hello.py
@@ -1,9 +1,10 @@
-print("hello546565656")
-print("bu")
+print("This is my first python program in pycharm")
+print("hello world!!! Changed!!!--")
 
 
 
 #exercise 1
 print("another print added!")
 print("just testing...")
+
 
---
+++ session-01/sum20.py
@@ -0,0 +1,12 @@
+#session 1 exercise 3
+#calculate the sum of the 20 first integer numbers
+#1+2+3+...+20
+#example working!
+
+#store the result
+res=0
+
+for i in range (1, 21):
+    res += i
+
+    print("total sum :", res)
